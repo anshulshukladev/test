eed8aa9dd41bb04f17d761e4341cf6e22d83eba6
Hiring Bonami <hiring@bonamisoftware.com>
2024-03-01 12:45:33 +0530

    First commit

--- test01/src/App.js
+++ test01/src/App.js
@@ -1,9 +1,11 @@
-function App() {
+import React from 'react'
+
+const App = () => {
   return (
-    <div className="App">
-      HI
+    <div>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
